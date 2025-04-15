939440463ff9bc40205b91d588d7ae27599ffcb0
shivam verma <shivam.verma@velocis.co.in>
2025-04-15 17:05:54 +0530

    Fixed bugs

--- app/Views/Reports/copying/report.php
+++ app/Views/Reports/copying/report.php
@@ -35,51 +35,48 @@
                             <div class="col-md-12">
                                 <div class="card-header p-2" style="background-color: #fff;">
                                     <ul class="nav nav-pills">
-                                        <li class="nav-item"><a onclick="get_search_view(1)" class="nav-link active" href="#AOR_signature_report" data-toggle="tab">AOR signature report</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(2)" class="nav-link <?php if(($uri->getSegment(4))=="consumed_barcodes_search"){ echo 'active';}?>" href="#Consumed_Barcodes" id="consumed_barcodes_search_click" data-toggle="tab">Consumed Barcodes</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(3)" class="nav-link <?php if(($uri->getSegment(4))=='copying_request_search'){ echo 'active';}?>" href="#CopyingRequest" id="copying_request_click" data-toggle="tab">Copying Request</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(4)" class="nav-link <?php if(($uri->getSegment(4))=='DA_wise_search_search'){ echo 'active';}?>" href="#DA_Wise" id="DA_wise_search_click" data-toggle="tab">DA Wise</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(5)" class="nav-link <?php if(($uri->getSegment(4))=='ecopy_stats_search'){ echo 'active';}?>" href="#EcopyStats" id="ecopy_stats_search_click" data-toggle="tab">E-Copy Stats</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(6)" class="nav-link <?php if(($uri->getSegment(4))=='ePay_search'){ echo 'active';}?>" href="#ePay" id="epay_search_click" data-toggle="tab">ePay</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(7)" class="nav-link <?php if(($uri->getSegment(4))=='file_request_search'){ echo 'active';}?>" href="#File_Request" id="file_request_search_click" data-toggle="tab">File Request</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(8)" class="nav-link <?php if(($uri->getSegment(4))=='received_by_r_and_i_search'){ echo 'active';}?>" href="#Received by R&amp;I" id="received_by_r_and_i_search_search_click" data-toggle="tab">Received by R&amp;I</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(9)" class="nav-link <?php if(($uri->getSegment(4))=='search_by_diary_number_search'){ echo 'active';}?>" href="#Search_by_Diary_Number" id="search_by_diary_number_search_click" data-toggle="tab">Search by Diary Number</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(10)" class="nav-link <?php if(($uri->getSegment(4))=='userwise_report_search'){ echo 'active';}?>" href="#Userwise_Report" id="userwise_report_search_click" data-toggle="tab">Userwise Report</a></li>
-                                        <li class="nav-item"><a onclick="get_search_view(11)" class="nav-link <?php if(($uri->getSegment(4))=='view_search'){ echo 'active';}?>" href="#View" id="view_search_click" data-toggle="tab">View</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(1)" class="nav-link <?php if($type=="1"){ echo 'active';}?>" href="#AOR_signature_report" data-toggle="tab">AOR signature report</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(2)" class="nav-link <?php if($type=="2"){ echo 'active';}?>" href="#Consumed_Barcodes" id="consumed_barcodes_search_click" data-toggle="tab">Consumed Barcodes</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(3)" class="nav-link <?php if($type=='3'){ echo 'active';}?>" href="#CopyingRequest" id="copying_request_click" data-toggle="tab">Copying Request</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(4)" class="nav-link <?php if($type=='4'){ echo 'active';}?>" href="#DA_Wise" id="DA_wise_search_click" data-toggle="tab">DA Wise</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(5)" class="nav-link <?php if($type=='5'){ echo 'active';}?>" href="#EcopyStats" id="ecopy_stats_search_click" data-toggle="tab">E-Copy Stats</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(6)" class="nav-link <?php if($type=='6'){ echo 'active';}?>" href="#ePay" id="epay_search_click" data-toggle="tab">ePay</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(7)" class="nav-link <?php if($type=='7'){ echo 'active';}?>" href="#File_Request" id="file_request_search_click" data-toggle="tab">File Request</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(8)" class="nav-link <?php if($type=='8'){ echo 'active';}?>" href="#Received by R&amp;I" id="received_by_r_and_i_search_search_click" data-toggle="tab">Received by R&amp;I</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(9)" class="nav-link <?php if($type=='9'){ echo 'active';}?>" href="#Search_by_Diary_Number" id="search_by_diary_number_search_click" data-toggle="tab">Search by Diary Number</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(10)" class="nav-link <?php if($type=='10'){ echo 'active';}?>" href="#Userwise_Report" id="userwise_report_search_click" data-toggle="tab">Userwise Report</a></li>
+                                        <li class="nav-item"><a onclick="get_search_view(11)" class="nav-link <?php if($type=='11'){ echo 'active';}?>" href="#View" id="view_search_click" data-toggle="tab">View</a></li>
                                     </ul>
                                 </div>
-        <div class="card-body">
-        <div class="tab-content">
-            <div id="load_search_view"> </div>
-
-   </div>
-    <!-- /.row -->
-  </div>
-    <!-- /.container-fluid -->
+							</div>	
+						<div class="card-body">
+							<div class="tab-content">
+								<div id="load_search_view"> </div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>	
+		</div>	
+	 </div>	
   </section>
-    <!-- /.content -->
+<script>
 
-    <script>
-
-        get_search_view(1);
+        get_search_view('<?=$type?>');
 
         function get_search_view(type) {
-            // var CSRF_TOKEN = 'CSRF_TOKEN';
-            // var CSRF_TOKEN_VALUE = $('[name="CSRF_TOKEN"]').val();
-
             $.ajax({
                 type: "GET",
                 data: { type: type},
                 url: "<?php echo base_url('Reports/Copying/Report/get_search_view'); ?>",
+				beforeSend: function() {
+					$('#load_search_view').html("<div style='margin:0 auto;margin-top:20px;width:15%'><img src='<?php echo base_url('images/load.gif'); ?>'></div>");
+				},
                 success: function (data)
                 {
                     $('#load_search_view').html(data);
-
-                  //  updateCSRFToken();
-                }
-                // error: function () {
-                //     updateCSRFToken();
-                // }
+				}
+                
             });
 
         }
