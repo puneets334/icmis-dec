3901b5b5eac5091819b349450029d7cda2e78195
Shubham140225 <shubham.agarwal@velocis.co.in>
2025-04-21 10:52:37 +0530

    lwdr reports

--- app/Views/lwdr/section_report.php
+++ app/Views/lwdr/section_report.php
@@ -1,5 +1,4 @@
 <?= view('header.php'); ?>
-
 <style>
     fieldset
     {
@@ -25,20 +24,30 @@
         background-color: #ffffff;
     }
 </style>
-<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-<body class="hold-transition skin-blue layout-top-nav">
-<div class="wrapper1" >
-    <!-- Full Width Column -->
-    <!--<div class="content-wrapper">
-        <div class="container">-->
-            <!-- Main content -->
-            <section class="content">
-            <form method="POST" action="section_report">
-                <fieldset class="fieldset">
-                    <legend class="legend">Specific Search</legend>
-                    <div class="row" style="text-align: center;">
-                        <div class="col-xs-4">
-                            <select name="section" class="form-control">
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 mt-3">
+
+                <div class="card">
+                    <div class="card-header heading">
+                        <div class="row">
+                            <div class="col-sm-10">
+                                <h3 class="card-title">FDR - Cash & Accounts</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+
+                        <form class="form-horizontal" id="push-form" method="post">
+                            <?= csrf_field(); ?>
+
+                            <div class="box-body">
+                                <div class="row">
+                                    <div class="col-sm-5">
+                                        <div class="form-group">
+                                            <label for="section" class="col-form-label">Specific Search</label>
+                                            <select name="section" class="form-control sel_sec">
                                 <option value="0">All Sections</option>
                                 <?php
                                 foreach ($sections as $val){
@@ -46,119 +55,59 @@
                                 }
                                 ?>
                             </select>
-                        </div><div class="col-xs-8" style="text-align: left;">
-                            <input type="submit" class="btn bg-olive btn-flat">
-                        </div></div></fieldset>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="col-sm-2 mt-4">
+                                        <div class="form-group">
+                                            <button type="submit" id="view" name="view" class="btn btn-primary mt-4">View</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
 
-                <br/>
-            </form>
-                <?php
-                    if(isset($report) && $report!=false){
-                ?>
-                <div class="box box-success">
-                    <div class="box-header with-border " style="text-align: center;">
-                        <h3 class="box-title" id="form-title" >LIST OF FIXED DEPOSIT PENDING CASES</h3>
+                        <img id="image" src="<?= base_url(); ?>/images/load.gif" style="display: none;">
+                        <div id="table_Result"></div>
                     </div>
-
-                    <table class="table table-striped table-hover "id="Sec_Report">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Case No</th>
-                            <th>Petitioner Name</th>
-                            <th>Respondent Name</th>
-                            <th>Section</th>
-                            <th>FDR/BG No.</th>
-                            <th>A/C No.</th>
-                            <th>Amount</th>
-                            <th>Bank</th>
-                            <th>Deposit Date</th>
-                            <th>Maturity/Expiry Date</th>
-                            <th>Payment Status</th>
-                            <th>Rate of Interest</th>
-                            <th>Dealing Assistant</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        setlocale(LC_MONETARY, 'en_IN');
-
-                        $sNo = 1;
-                        foreach($report as $row){
-
-                            echo "<tr>
-                                <td>$sNo</td>   
-                                   <td>".$row['case_number_display']."</td>
-                                   <td>".$row['petitioner_name']."</td>                  
-                                   <td>".$row['respondent_name']."</td>
-                                <td>".$row['section_name']."</td>
-                                <td>".$row['document_number']."</td>
-                                <td>".$row['account_number']."</td>
-                                <td>".money_format('%!i', $row['amount'])."</td>
-                                <td>".$row['bank_name']."</td>
-                                <td>".date('d-m-Y', strtotime($row['deposit_date']))."</td>
-                                <td>".date('d-m-Y', strtotime($row['maturity_date']))."</td> 
-                                <td>".$row['status']."</td>
-                                <td>".$row['roi']."</td>
-                                <td>".$row['da']."</td>
-                              </tr>";
-                            $sNo++;
-                        }
-                        ?>
-                        </tbody>
-                    </table>
                 </div>
-                <?php }
-                else if(isset($report))
-                    echo "No record found";?>
-            </section>
-            <!-- /.content -->
-        <!--</div>-->
-        <!-- /.container -->
-    <!--</div>-->
-    <!-- /.content-wrapper -->
-
-</div>
-<script src="<?=base_url()?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
-
-<script src="<?=base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
-
-
-<script src="<?=base_url()?>assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<script src="<?=base_url()?>assets/js/bootstrap.min.js"></script>
-<script src="<?=base_url()?>assets/plugins/fastclick/fastclick.js"></script>
-<script src="<?=base_url()?>assets/js/app.min.js"></script>
-<script src="<?=base_url()?>assets/js/demo.js"></script>
-<script src="<?=base_url()?>assets/jsAlert/dist/sweetalert.min.js"></script>
-<script src="<?=base_url()?>assets/plugins/datepicker/bootstrap-datepicker.js"></script>
-
-<script src="<?=base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
-<script src="<?=base_url(); ?>assets/plugins/datatables/dataTables.buttons.min.js"></script>
-<script src="<?=base_url(); ?>assets/plugins/datatables/buttons.print.min.js"></script>
-
+            </div>
+        </div>
+    </div>
+</section>
 <script>
+ 
+    $('#push-form').on('submit', function(e) {
 
-    $('#Sec_Report').DataTable({
-        "paging": true,
-        "lengthChange": true,
-        "searching": true,
-        "ordering": true,
-        "info": true,
-        "autoWidth": true,
-        "scrollY": "50vh",
-        "scrollX": true,
-        "scrollCollapse": true,
-        "footerCallback":"",
-        dom: 'Blfrtip',
-        buttons: [
-            'print'
-        ]
-    });
+        var section = $('.sel_sec').val();
 
-    $('.datepicker').datepicker({
-        format: 'yyyy-mm-dd',
-        autoclose:'true'
+        if (section == '') {
+            alert("Please Select Specific Search");
+            return false;
+        }
+        e.preventDefault();
+        var formData = $(this).serialize();
+        $.ajax({
+            url: '<?= base_url(); ?>/lwdr/section_report_data',
+            type: 'POST',
+            data: formData,
+            success: function(response) {
+                $('#table_Result').html(response);
+            },
+            beforeSend: function() {
+                $('#table_Result').html('');
+                $('#view').attr('disabled', true);
+                $('#image').show();
+            },
+            complete: function() {
+                $('#view').attr('disabled', false);
+                $('#image').hide();
+                updateCSRFToken();
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+                alert('An error occurred while submitting the form.');
+            }
+        });
     });
 </script>
-</body>
-</html>
